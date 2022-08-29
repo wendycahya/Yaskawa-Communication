e41442ebbb7bf75960bddecdce9b726ffaa3f83d
Wendy C. Kurniawan <47620821+wendycahya@users.noreply.github.com>
2022-08-29 12:16:12 +0800

    update

--- gui/7-MovementScenario.py
+++ gui/7-MovementScenario.py
@@ -21,7 +21,7 @@ def convert_mm(x, y, z, rx, ry, rz, re):
     rz = float(str_rz)
     re = float(str_re)
 
-    input = (x, y, z, rx, ry, rz, re)
+    input = [x, y, z, rx, ry, rz, re]
     return input
 
 def move_convert(post_original):
@@ -42,13 +42,13 @@ def move_distance(post1, post2):
     post1 = list(post1)
     post2 = list(post2)
 
-    x_coor = (post2[0] - post1[0])
-    y_coor = (post2[1] - post1[1])
-    z_coor = (post2[2] - post1[2])
-    rx_coor = (post2[3] - post1[3])
-    ry_coor = (post2[4] - post1[4])
-    rz_coor = (post2[5] - post1[5])
-    re_coor = (post2[6] - post1[6])
+    x_coor = post2[0] - post1[0]
+    y_coor = post2[1] - post1[1]
+    z_coor = post2[2] - post1[2]
+    rx_coor = post2[3] - post1[3]
+    ry_coor = post2[4] - post1[4]
+    rz_coor = post2[5] - post1[5]
+    re_coor = post2[6] - post1[6]
 
     dist = math.sqrt(math.pow((post2[0] - post1[0]), 2) + math.pow((post2[1] - post1[1]), 2) + math.pow((post2[2] - post1[2]), 2))
 
@@ -58,19 +58,20 @@ def move_distance(post1, post2):
 def time_robot(speed, distance):
     distance = distance / 1000
     speed = speed / 10
-    time = (distance / speed) + 0.5
+    time = round((distance / speed) + 0.5, 1)
     return time
 
 def rob_command(post1):
     x_coor = post1[0] * 1000
     y_coor = post1[1] * 1000
     z_coor = post1[2] * 1000
-    rx_coor = post1[3] * 1000
-    ry_coor = post1[4] * 1000
-    rz_coor = post1[5] * 1000
-    re_coor = post1[6] * 1000
+    # rx_coor = post1[3] * 1000
+    # ry_coor = post1[4] * 1000
+    # rz_coor = post1[5] * 1000
+    # re_coor = post1[6] * 1000
 
-    robot_command = (int(x_coor), int(y_coor), int(z_coor), int(rx_coor), int(ry_coor), int(rz_coor), int(re_coor))
+    #robot_command = (int(x_coor), int(y_coor), int(z_coor), int(rx_coor), int(ry_coor), int(rz_coor), int(re_coor))
+    robot_command = (int(x_coor), int(y_coor), int(z_coor), 0, 0, 0, 0)
     return robot_command
 
 def update_pos():
@@ -103,7 +104,7 @@ robot_no = 1
 post_1 = [0, 0, 0, 0, 0, 0, 0]
 if FS100.ERROR_SUCCESS == robot.read_position(pos_info, robot_no):
     x, y, z, rx, ry, rz, re = pos_info['pos']
-    post_ori = (x, y, z, rx, ry, rz, re)
+    post_ori = convert_mm(x, y, z, rx, ry, rz, re)
     str = "CURRENT POSITION\n" + \
           "COORDINATE {:12s} TOOL:{:02d}\n".format('ROBOT', pos_info['tool_no']) + \
           "R{} :X     {:4d}.{:03d} mm       Rx   {:4d}.{:04d} deg.\n".format(robot_no,
@@ -140,7 +141,8 @@ status = {}
 #post_1 = (457413, -5034, 293332, 1786969, -24, -303, 0)
 
 #===== point movement mm =====
-pointHome = [457.413, -5.049, 293.318, 178.6969, -0.0019, -0.0283, 0]
+#pointHome = [457.413, -5.049, 293.318, 178.6969, -0.0019, -0.0283, 0]
+pointHome = post_ori
 point1 = [405.916, -387.580, 345.821, 178.6969, -0.0029, -0.0306, 0]
 point2 = [405.929, -387.592, -201.391, 178.7000, 0.0002, -0.0261, 0]
 point4 = [405.919, 253.778, 345.819, 178.6967, -0.0031, -0.0308, 0]
@@ -154,7 +156,8 @@ rob1 = rob_command(point1)
 rob2 = rob_command(point2)
 rob4 = rob_command(point4)
 rob5 = rob_command(point5)
-
+print("robot home position")
+print(robHome)
 # ===== move and distance =========
 post1_move, distance1 = move_distance(robHome, rob1)
 post2_move, distance2 = move_distance(rob1, rob2)
@@ -164,60 +167,28 @@ post5_move, distance5 = move_distance(rob4, rob5)
 post6_move, distance6 = move_distance(rob5, rob4)
 post7_move, distance7 = move_distance(rob4, robHome)
 
-# ===== list movement task ========
+#servo on check
+if FS100.ERROR_SUCCESS == robot.get_status(status):
+    if not status['servo_on']:
+        robot.switch_power(FS100.POWER_TYPE_SERVO, FS100.POWER_SWITCH_ON)
 
-# post_1 = post_ori
-# print("Post 1: ", post_1)
-# post_2 = post_1
-# print("Post 2[2]: ", post_2[2])
-# lst = list(post_2)
-# lst[2] = lst[2] + 50000
-# post_2 = tuple(lst)
-# print("Post new post 2: ", post_2)
-# # post_2[2] = post_2[2] + 10000
-# print("Post 2: ", post_2)
-#
-# post_3 = post_2
-# lst = list(post_3)
-# lst[2] = lst[2] + 50000
-# post_3 = tuple(lst)
-#
-# post_4 = post_3
-# lst = list(post_4)
-# lst[1] = lst[1] + 50000
-# post_4 = tuple(lst)
-#
-# print("Post ori before move: ", post_ori)
-# print("Post 1 before move: ", post_1)
-# print("Post 2 before move: ", post_2)
-# print("Post 2 before move: ", post_3)
-# print("Post 2 before move: ", post_4)
-#
-# #list_move = [pos1_move, pos2_move, pos3_move]
-#
-# if FS100.ERROR_SUCCESS == robot.get_status(status):
-#     if not status['servo_on']:
-#         robot.switch_power(FS100.POWER_TYPE_SERVO, FS100.POWER_SWITCH_ON)
-#
+# ===== list movement task ========
 pos_updater = threading.Thread(target=update_pos)
 index = 0
 tredON = False
-#
-# move1, distance1 = move_distance(post_1, post_2)
-# move2, distance2 = move_distance(post_2, post_3)
-# move3, distance3 = move_distance(post_3, post_4)
-# print("list move 1: ", move1)
-# print("========================")
-# print("list move 2: ", move2)
-# print("=========================")
-# print("list move 2: ", move3)
-# print("=========================")
-# list_move = [move1, move2, move3]
-# dist = [distance1, distance2, distance3]
-# print(dist)
 
 postMove = [post1_move, post2_move, post3_move, post4_move, post5_move, post6_move, post7_move]
 dist = [distance1, distance2, distance3, distance4, distance5, distance6, distance7]
+
+print("move 1: ", post1_move, "cek distance ", distance1)
+print("move 2: ", post2_move, "cek distance ", distance2)
+print("move 3: ", post3_move, "cek distance", distance3)
+print("move 4: ", post4_move, "cek distance", distance4)
+print("move 5: ", post5_move, "cek distance", distance5)
+print("move 6: ", post6_move, "cek distance", distance6)
+print("move 7: ", post7_move, "cek distance", distance7)
+
+
 
 for x in postMove:
 #declare the post
@@ -226,6 +197,7 @@ for x in postMove:
 #time calculation
     time_d = time_robot(speed, dist[index])
     print(time_d)
+    print("nilai x yang masuk ", index, "sebesar ", x)
     if FS100.ERROR_SUCCESS == robot.one_move(FS100.MOVE_TYPE_LINEAR_INCREMENTAL_POS,FS100.MOVE_COORDINATE_SYSTEM_ROBOT, speed_class, speed, x):
         time.sleep(time_d)  # robot may not update the status
         if not is_alarmed() and tredON == False:
