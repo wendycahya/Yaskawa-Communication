899997b715f0491aafa05e52a0f765e3e26b6461
Wendy C. Kurniawan <47620821+wendycahya@users.noreply.github.com>
2023-06-27 10:58:05 +0800

    update

---
+++ IntegratedSystem/Basic Function Test/pyserialScanInput.py
@@ -0,0 +1,10 @@
+import serial
+
+port = '/dev/ttyUSB0'  # Linux
+# port = 'COM1'  # Windows
+baud_rate = 9600
+
+ser = serial.Serial(port, baud_rate)
+command = '1'  # Example command
+ser.write(command.encode())
+ser.close()
