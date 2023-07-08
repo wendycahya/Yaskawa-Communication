ef8a9688c1bde9a1d1c081ea2093948a3ae8e051
Wendy C. Kurniawan <47620821+wendycahya@users.noreply.github.com>
2023-07-08 15:38:24 +0800

    update

--- IntegratedSystem/Basic Function Test/python-arduino-communication.py
+++ IntegratedSystem/Basic Function Test/python-arduino-communication.py
@@ -1,15 +1,21 @@
+import time
+
 import serial
 
 # Configure the serial port
 ser = serial.Serial('/dev/ttyUSB0', 9600)  # Replace 'COM3' with your port name
 
+ser.write('0'.encode())
+time.sleep(1)
 # Read data from Arduino
 while True:
-    #message = "1"
+    # message = '1'
     message = input("Enter a message to send to Arduino: ")
+    # ser.write('1'.encode())
     ser.write(message.encode())
-    print("nilai message= ", message)
-    line = ser.readline().decode('latin-1').rstrip()
-    print(line)
+    # print("nilai message= ", message)
+    # line = ser.readline().decode('latin-1').rstrip()
+    # print(line)
+    time.sleep(1)
 # Close the serial port connection
 ser.close()
